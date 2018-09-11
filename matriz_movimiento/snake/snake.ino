fb921ef0c8586fed7185aa3980c52437d266a202
petrlr14 <pedrodeveloper14@gmail.com>
2018-09-10 21:52:41 -0600

    se logro movimiento con tiempo

--- matriz_movimiento/snake/snake.ino
+++ matriz_movimiento/snake/snake.ino
@@ -1,12 +1,15 @@
 int xEnemy=5;
 int yEnemy=5;
+int currentPosX=0, currentPosY=0;
+unsigned long currentTime, startTime=0;
 int matriz[6][6] = {
     {0, 0, 0, 0, 0, 0},
-     {0, 0, 0, 0, 0, 0},
-     {0, 0, 0, 0, 0, 0},
-     {0, 0, 0, 0, 0, 0},
-     {0, 0, 0, 0, 0, 0},
-     {0, 0, 0, 0, 0, 0}};
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0}
+};
 
 void limpiar()
 {
@@ -37,24 +40,57 @@ void cleanMatrix(){
     }
 }
 
+void updateMatrix(){
+    cleanMatrix();
+    int x=map(analogRead(A0), 0, 1023, 0, 5);
+    int y=map(analogRead(A1), 0, 1023, 0, 5);
+    switch(x){
+        case 0: currentPosX-=5;
+                break;
+        case 1: currentPosX-=1;
+                break;
+        case 4: currentPosX+=1;
+                break;
+        case 5: currentPosX+=5;
+                break;
+    }
+    switch(y){
+        case 0: currentPosY-=5;
+                break;
+        case 1: currentPosY-=1;
+                break;
+        case 4: currentPosY+=1;
+                break;
+        case 5: currentPosY+=5;
+                break;
+    }
+    currentPosX=constrain(currentPosX, 0, 5000);
+    currentPosY=constrain(currentPosY, 0, 5000);
+    matriz[currentPosY/1000][currentPosX/1000]=1;
+}
+
 void loop()
 {
-    int x=map(analogRead(A0), 0, 1023, 1, 6);
-    int y=map(analogRead(A1), 0, 1023, 1, 6);
     
-        
-        for(int i=0; i<50; i++){
-            for(int j=0; j<6; j++){
-                digitalWrite(j, HIGH);
-                cleanMatrix();
-                matriz[x-1][y-1]=1;
-                for(int k=6; k<12; k++){
-                    if(matriz[j][k-6]==1)  {
-                        digitalWrite(k, LOW);
-                    } 
+    
+    currentTime=millis();
+    if(currentTime>=startTime+6000){
+        xEnemy=random(0, 5);
+        yEnemy=random(0, 5);
+        startTime=currentTime;
+    }
+    for(int i=0; i<500; i++){
+        for(int j=0; j<6; j++){
+            digitalWrite(j, HIGH);
+            for(int k=6; k<12; k++){
+                if(matriz[j][k-6]==1){
+                    digitalWrite(k, LOW);
                 }
-                limpiar();
             }
+            limpiar();
+            
+            updateMatrix();
         }
-    
+    }
+        
 }
